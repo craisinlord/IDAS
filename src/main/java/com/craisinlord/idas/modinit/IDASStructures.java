d7b0da4ddce6bf37bc435b8415a4a4d47b86be40
crasinlord <craisinlord@gmail.com>
2022-09-05 13:34:15 -0500

    First IDAS Source Code

--- src/main/java/com/craisinlord/idas/modinit/IDASStructures.java
+++ src/main/java/com/craisinlord/idas/modinit/IDASStructures.java
@@ -5,8 +5,10 @@ import com.craisinlord.idas.IDAS;
 import com.craisinlord.idas.world.structures.GenericJigsawStructure;
 import com.craisinlord.idas.world.structures.GenericNetherJigsawStructure;
 import com.craisinlord.idas.world.structures.GenericEndJigsawStructure;
+import com.craisinlord.idas.world.structures.OverLavaNetherStructure;
 import com.craisinlord.idas.world.structures.configs.IDASGenericConfig;
 import com.craisinlord.idas.world.structures.configs.IDASGenericNetherConfig;
+import com.craisinlord.idas.world.structures.configs.IDASOverLavaNetherConfig;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.feature.StructureFeature;
 import net.minecraftforge.registries.DeferredRegister;
@@ -22,11 +24,13 @@ public final class IDASStructures {
     public static RegistryObject<StructureFeature<?>> GENERIC_JIGSAW_STRUCTURE = STRUCTURE_FEATURES.register("idas_jigsaw_structure", () -> new GenericJigsawStructure<>(IDASGenericConfig.CODEC));
     public static RegistryObject<StructureFeature<?>> GENERIC_UNDERGROUND_JIGSAW_STRUCTURE = STRUCTURE_FEATURES.register("idas_underground_jigsaw_structure", () -> new GenericJigsawStructure<>(IDASGenericConfig.CODEC));
     public static RegistryObject<StructureFeature<?>> GENERIC_NETHER_JIGSAW_STRUCTURE = STRUCTURE_FEATURES.register("idas_nether_jigsaw_structure", () -> new GenericNetherJigsawStructure<>(IDASGenericNetherConfig.CODEC));
+    public static RegistryObject<StructureFeature<?>> OVER_LAVA_NETHER_STRUCTURE = STRUCTURE_FEATURES.register("idas_over_lava_nether_structure", () -> new OverLavaNetherStructure<>(IDASOverLavaNetherConfig.CODEC));
     public static RegistryObject<StructureFeature<?>> GENERIC_END_JIGSAW_STRUCTURE = STRUCTURE_FEATURES.register("idas_end_jigsaw_structure", () -> new GenericEndJigsawStructure<>(IDASGenericConfig.CODEC));
     public static void setupStructures() {
         StructureFeatureAccessor.getSTEP().put(GENERIC_JIGSAW_STRUCTURE.get(), GenerationStep.Decoration.SURFACE_STRUCTURES);
         StructureFeatureAccessor.getSTEP().put(GENERIC_UNDERGROUND_JIGSAW_STRUCTURE.get(), GenerationStep.Decoration.UNDERGROUND_STRUCTURES);
         StructureFeatureAccessor.getSTEP().put(GENERIC_NETHER_JIGSAW_STRUCTURE.get(), GenerationStep.Decoration.SURFACE_STRUCTURES);
+        StructureFeatureAccessor.getSTEP().put(OVER_LAVA_NETHER_STRUCTURE.get(), GenerationStep.Decoration.SURFACE_STRUCTURES);
         StructureFeatureAccessor.getSTEP().put(GENERIC_END_JIGSAW_STRUCTURE.get(), GenerationStep.Decoration.SURFACE_STRUCTURES);
 
         //registers the structure pieces.
---
+++ src/main/java/com/craisinlord/idas/world/structures/OverLavaNetherStructure.java
@@ -0,0 +1,72 @@
+package com.craisinlord.idas.world.structures;
+
+import com.craisinlord.idas.utils.GeneralUtils;
+import com.craisinlord.idas.world.structures.configs.IDASOverLavaNetherConfig;
+import com.craisinlord.idas.world.structures.pieces.PieceLimitedJigsawManager;
+import com.mojang.serialization.Codec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.NoiseColumn;
+import net.minecraft.world.level.levelgen.feature.configurations.JigsawConfiguration;
+import net.minecraft.world.level.levelgen.structure.StructureSet;
+import net.minecraft.world.level.levelgen.structure.pieces.PieceGenerator;
+import net.minecraft.world.level.levelgen.structure.pieces.PieceGeneratorSupplier;
+
+import java.util.Optional;
+
+
+public class OverLavaNetherStructure<C extends IDASOverLavaNetherConfig> extends AbstractBaseStructure<C> {
+
+    public OverLavaNetherStructure(Codec<C> codec) {
+        super(codec, OverLavaNetherStructure::isOverLavaNetherFeatureChunk, OverLavaNetherStructure::generateOverLavaNetherPieces);
+    }
+
+    protected static <CC extends IDASOverLavaNetherConfig> boolean isOverLavaNetherFeatureChunk(PieceGeneratorSupplier.Context<CC> context) {
+        // Check to see if there some air where the structure wants to spawn.
+        // Doesn't account for rotation of structure.
+        ChunkPos chunkPos = context.chunkPos();
+        BlockPos blockPos = new BlockPos(chunkPos.getMinBlockX(), context.chunkGenerator().getSeaLevel() + 1, chunkPos.getMinBlockZ());
+        CC config = context.config();
+
+        int checkRadius = 16;
+        BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos();
+
+        for(int xOffset = -checkRadius; xOffset <= checkRadius; xOffset += 8) {
+            for(int zOffset = -checkRadius; zOffset <= checkRadius; zOffset += 8) {
+                NoiseColumn blockView = context.chunkGenerator().getBaseColumn(xOffset + blockPos.getX(), zOffset + blockPos.getZ(), context.heightAccessor());
+                for(int yOffset = 0; yOffset <= 30; yOffset += 5) {
+                    mutable.set(blockPos).move(xOffset, yOffset, zOffset);
+                    if (!blockView.getBlock(mutable.getY()).isAir()) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        //cannot be near other specified structure
+        for (ResourceKey<StructureSet> structureSetToAvoid : config.structureSetToAvoid) {
+            if (context.chunkGenerator().hasFeatureChunkInRange(structureSetToAvoid, context.seed(), chunkPos.x, chunkPos.z, config.structureAvoidRadius)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static <CC extends IDASOverLavaNetherConfig> Optional<PieceGenerator<CC>> generateOverLavaNetherPieces(PieceGeneratorSupplier.Context<CC> context) {
+        CC config = context.config();
+        BlockPos blockpos = new BlockPos(context.chunkPos().getMinBlockX(), context.chunkGenerator().getSeaLevel() + config.sealevelOffset, context.chunkPos().getMinBlockZ());
+
+        return PieceLimitedJigsawManager.assembleJigsawStructure(
+                context,
+                new JigsawConfiguration(config.startPool, config.size),
+                GeneralUtils.getCsfNameForConfig(config, context.registryAccess()),
+                blockpos,
+                false,
+                false,
+                Integer.MAX_VALUE,
+                Integer.MIN_VALUE,
+                (structurePiecesBuilder, pieces) -> GeneralUtils.centerAllPieces(blockpos, pieces));
+    }
+}
